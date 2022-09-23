e5d0238eb46bcd0ffbf3fbab27704c3a6a36da82
zooyaho <jiwoo1268@naver.com>
2022-09-23 20:40:01 +0900

    feat: Add signup done page publishing

--- src/components/SignUpDonePage/SignUpDonePage.tsx
+++ src/components/SignUpDonePage/SignUpDonePage.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Box, ChakraProps, Button, Flex, Image, Text } from '@chakra-ui/react';
+import { ChakraProps, Button, Flex, Text, Center, Spacer } from '@chakra-ui/react';
+import { HandsClappingIcon } from 'generated/icons/MyIcons';
 
-interface SignUpDonePageProps extends ChakraProps {}
+interface SignUpDonePageProps extends ChakraProps { }
 
 function SignUpDonePage({ ...basisProps }: SignUpDonePageProps) {
   return (
-    <Box {...basisProps}>
-      <Text>SignUpDonePage</Text>
-    </Box>
+    <Flex as='main' h='100vh' px='1rem' flexDirection='column'>
+      <Text as='h2' textStyle='xl' fontWeight='bold' pt='80px'>회원가입이<br />
+        완료되었습니다.</Text>
+      <Text as='p' mt='5px' textStyle='sm' color='gray.600'>관심사별로 자유롭게 소통해보세요!</Text>
+      <Spacer />
+      <Center position='absolute' top='35vh' left='0' right='0'>
+        <HandsClappingIcon />
+      </Center>
+      <Spacer />
+      <Button w='100%' size='lg' fontWeight='bold' borderRadius='25px' mb='1.875rem' colorScheme='primary' >
+        시작하기
+      </Button>
+    </Flex>
   );
 }
 
--- src/pages/sign-up-done/index.tsx
+++ src/pages/sign-up-done/index.tsx
@@ -1,15 +1,14 @@
 import Head from 'next/head';
-import HomeLayout from '@components/common/@Layout/HomeLayout';
 import SignUpDonePage from '@components/SignUpDonePage';
+import CardLayout from '@components/common/@Layout/CardLayout';
 
 function SignUpDone() {
   return (
     <>
       <Head>
-        {/* ex) Your App Name | Page Name */}
-        <title>인코스런 커머스트랙 | sign-up-done</title>
+        <title>Beauty Core | sign-up-done</title>
       </Head>
-      <HomeLayout content={<SignUpDonePage />} />
+      <CardLayout content={<SignUpDonePage />} />
     </>
   );
 }

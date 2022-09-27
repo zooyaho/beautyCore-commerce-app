3546633474b8224998ad867488eea3ee43e47b05
zooyaho <jiwoo1268@naver.com>
2022-09-27 21:13:52 +0900

    feat: Add rating component

---
+++ src/components/common/RatingStar/RatingStar.tsx
@@ -0,0 +1,27 @@
+import { useCallback } from 'React';
+
+import { RatingStarIcon } from 'generated/icons/MyIcons';
+
+interface IProps {
+  rate: number;
+  value: number;
+  onChange: (v: number) => void;
+}
+
+function RatingStar({ rate, value, onChange }: IProps) {
+  const onClickIcon = useCallback(() => {
+    onChange(rate);
+  }, [onChange, rate]);
+
+  return (
+    <RatingStarIcon
+      boxSize="24px"
+      m="6px"
+      color={rate <= value ? 'primary.500' : 'gray:400'}
+      cursor="pointer"
+      onClick={onClickIcon}
+    />
+  );
+}
+
+export default RatingStar;

efcc208bbe49269d94d4159530de64b989fe7e30
zooyaho <jiwoo1268@naver.com>
2022-09-28 16:08:37 +0900

    feat: Add order page publishing

---
+++ src/components/OrderPage/OrderPage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import OrderPageView from './OrderPage.view';
+import useFormValidate from './_hooks/useFormValidate';
+
+// interface SignUpPageProps extends ChakraProps { }
+
+const OrderPage = () => {
+  const formData = useFormValidate();
+  const { handleSubmit } = formData;
+  console.log('formData: ', formData);
+
+  const onSubmit = handleSubmit(
+    ({
+      username,
+      phone,
+      adress,
+      adressDetail,
+      orderUsername,
+      orderPhone,
+      orderAdress,
+      orderAdressDetail,
+      orderRequest,
+    }) => {
+      console.log(
+        `submitted: ${username},  ${phone}, ${adress}, ${adressDetail}, ${orderUsername},  ${orderPhone}, ${orderAdress}, ${orderAdressDetail},${orderRequest}`,
+      );
+    },
+  );
+  return <OrderPageView formData={formData} onSubmit={onSubmit} />;
+};
+
+export default OrderPage;

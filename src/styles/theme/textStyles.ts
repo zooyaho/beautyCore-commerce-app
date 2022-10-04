1de05334ea1517ab0d8f425b7008aef0147026b5
zooyaho <jiwoo1268@naver.com>
2022-10-04 12:57:59 +0900

    feat: Add text style
    - 재사용 많은 text style 추가

--- src/styles/theme/textStyles.ts
+++ src/styles/theme/textStyles.ts
@@ -15,6 +15,177 @@ const textStyles = {
     fontSize: '26px',
     lineHeight: '38px',
   },
+  /* size weight */
+  ss_wb: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: '700',
+  },
+  sm_wb: {
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: '700',
+  },
+  sl_wb: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: '700',
+  },
+  sxl_wb: {
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: '700',
+  },
+  /* size weight-normal color-primary.500 */
+  ss_wn_cp: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: '400',
+    textColor: 'primary.500',
+  },
+  sm_wn_cp: {
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: '400',
+    textColor: 'primary.500',
+  },
+  sl_wn_cp: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: '400',
+    textColor: 'primary.500',
+  },
+  sxl_wn_cp: {
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: '400',
+    textColor: 'primary.500',
+  },
+  /* size weight-bold color-primary.500 */
+  ss_wb_cp: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: '700',
+    textColor: 'primary.500',
+  },
+  sm_wb_cp: {
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: '700',
+    textColor: 'primary.500',
+  },
+  sl_wb_cp: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: '700',
+    textColor: 'primary.500',
+  },
+  sxl_wb_cp: {
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: '700',
+    textColor: 'primary.500',
+  },
+  /* size weight-normal color-gray.600 */
+  ss_wn_cg600: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: '400',
+    textColor: 'gray.600',
+  },
+  sm_wn_cg600: {
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: '400',
+    textColor: 'gray.600',
+  },
+  sl_wn_cg600: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: '400',
+    textColor: 'gray.600',
+  },
+  sxl_wn_cg600: {
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: '400',
+    textColor: 'gray.600',
+  },
+  /* size weight-bold color-gray.600 */
+  ss_wb_cg600: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: '700',
+    textColor: 'gray.600',
+  },
+  sm_wb_cg600: {
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: '700',
+    textColor: 'gray.600',
+  },
+  sl_wb_cg600: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: '700',
+    textColor: 'gray.600',
+  },
+  sxl_wb_cg600: {
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: '700',
+    textColor: 'gray.600',
+  },
+  /* size weight-normal color-gray.700 */
+  ss_wn_cg700: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: '400',
+    textColor: 'gray.700',
+  },
+  sm_wn_cg700: {
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: '400',
+    textColor: 'gray.700',
+  },
+  sl_wn_cg700: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: '400',
+    textColor: 'gray.700',
+  },
+  sxl_nb_cg700: {
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: '400',
+    textColor: 'gray.700',
+  },
+  /* size weight-bold color-gray.700 */
+  ss_wb_cg700: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: '700',
+    textColor: 'gray.700',
+  },
+  sm_wb_cg700: {
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: '700',
+    textColor: 'gray.700',
+  },
+  sl_wb_cg700: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: '700',
+    textColor: 'gray.700',
+  },
+  sxl_wb_cg700: {
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: '700',
+    textColor: 'gray.700',
+  },
 };
 
 export default textStyles;

dcc39512e21b1fbbdf89d3fb267c6e070c4e535b
zooyaho <jiwoo1268@naver.com>
2022-10-05 15:07:43 +0900

    feat: Add image attachment and deletion functions

--- src/components/ReviewWritePage/ReviewWritePage.view.tsx
+++ src/components/ReviewWritePage/ReviewWritePage.view.tsx
@@ -9,16 +9,23 @@ import {
   Container,
   Divider,
   Flex,
+  Image,
   Img,
   Spacer,
   Text,
   Textarea,
 } from '@chakra-ui/react';
 
+import {
+  useUploadFileToS3Mutation,
+  useUploadFilesToS3Mutation,
+} from '@apis/S3FileUploader/S3FileUploaderApi.mutation';
+
 import FormHelper from '@components/common/FormHelper';
 import RatingStars from '@components/common/InputRatingStars';
 
 import { LAYOUT } from '@constants/layout';
+import { bytesToMB, fileToBase64, isBase64Img, isOverSize } from '@utils/file';
 
 import { FormDataType } from './_hooks/useFormValidate';
 
@@ -39,6 +46,37 @@ function RiewviewWritePageView({
   onSubmit,
   ...basisProps
 }: FormPageProps) {
+  const [files, setFiles] = React.useState<File[]>([]); // 파일 상태
+  const [currentFile, setCurrentFile] = React.useState<File | null>(null);
+  const [currentFileBase64, setCurrentFileBase64] = React.useState<
+    string | ArrayBuffer | null
+  >();
+  const [printImgsSrc, setPrintImgsSrc] = React.useState<string[]>([]);
+
+  const onChangeFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+    setCurrentFile(file); // 최근 파일 저장
+    setFiles((cur) => [...cur, file]); // 파일 목록에 저장
+  };
+
+  // For: Convert Current File To Base64
+  React.useEffect(() => {
+    async function setter() {
+      if (!currentFile) return;
+      setCurrentFileBase64(await fileToBase64(currentFile));
+    }
+    setter();
+  }, [currentFile]);
+
+  React.useEffect(() => {
+    if (!currentFileBase64) return;
+    if (printImgsSrc.length >= 3) return;
+    if (typeof currentFileBase64 === 'string')
+      setPrintImgsSrc((src) => [...src, currentFileBase64]);
+  }, [currentFileBase64]);
+
   return (
     <Box pt={LAYOUT.HEADER.HEIGHT}>
       <Text as="h2" textStyle="sl_wb" mt="1.6rem" px="1rem">
@@ -119,49 +157,49 @@ function RiewviewWritePageView({
         />
         <Divider />
         <Text my="1.3rem">
-          사진첨부&nbsp;<Text as="span">(0/3)</Text>
+          사진첨부&nbsp;<Text as="span">({printImgsSrc.length}/3)</Text>
         </Text>
         <Flex gap="10px" pt=".7rem">
           {/* 사진 추가 버튼 */}
-          <Button variant="transparentButton" pt=".7rem" mr=".6rem">
-            <Center p="2rem" border="1px dashed #CBCED6" borderRadius="5px">
-              <PlusIcon color="gray.400" boxSize="18px" />
-            </Center>
-          </Button>
+          {printImgsSrc.length === 3 ? null : (
+            <Button
+              as="label"
+              variant="transparentButton"
+              pt=".7rem"
+              mr=".6rem"
+            >
+              <Center p="2rem" border="1px dashed #CBCED6" borderRadius="5px">
+                <input
+                  style={{ display: 'none' }}
+                  type="file"
+                  onChange={onChangeFile}
+                />
+                <PlusIcon color="gray.400" boxSize="18px" />
+              </Center>
+            </Button>
+          )}
           {/* s: 이미지 */}
-          <Flex pos="relative" pt="10px" pr="10px">
-            <Img
-              src="./images/dummyImg/Rectangle_2008.png"
-              alt="상품 이미지"
-              w="5.3rem"
-              h="5.3rem"
-            />
-            <Button
-              variant="transparentButton"
-              pos="absolute"
-              top="0"
-              right="0"
-            >
-              <PlusItemIcon color="gray.400" boxSize="20px" />
-            </Button>
-          </Flex>
+          {printImgsSrc.reverse().map((src, i) => {
+            return (
+              <Flex key={i} pos="relative" pt="10px" pr="10px">
+                <Image src={src} alt="상품 이미지" w="5.3rem" h="5.3rem" />
+                <Button
+                  variant="transparentButton"
+                  pos="absolute"
+                  top="0"
+                  right="0"
+                  onClick={() => {
+                    setPrintImgsSrc(() =>
+                      printImgsSrc.filter((printSrc, printI) => i !== printI),
+                    );
+                  }}
+                >
+                  <PlusItemIcon color="gray.400" boxSize="20px" />
+                </Button>
+              </Flex>
+            );
+          })}
           {/* e: 이미지 */}
-          <Flex pos="relative" pt="10px" pr="10px">
-            <Img
-              src="./images/dummyImg/Rectangle_2008.png"
-              alt="상품 이미지"
-              w="5.3rem"
-              h="5.3rem"
-            />
-            <Button
-              variant="transparentButton"
-              pos="absolute"
-              top="0"
-              right="0"
-            >
-              <PlusItemIcon color="gray.400" boxSize="20px" />
-            </Button>
-          </Flex>
         </Flex>
         {/* Submit Button */}
         <Button

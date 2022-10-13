1f35c4a93615a0a0300336cc66a75151430647b1
zooyaho <jiwoo1268@naver.com>
2022-10-13 13:25:00 +0900

    feat: Implementation of product infinite scroll

--- src/components/ProductListPage/ProductListPage.tsx
+++ src/components/ProductListPage/ProductListPage.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import { Box, Button, Container, Flex, Img, Text } from '@chakra-ui/react';
+
+import instance from '@apis/_axios/instance';
 
 import ScrollToTop from '@components/common/ScrollToTop';
 
 import { LAYOUT } from '@constants/layout';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 import { RatingStarIcon } from 'generated/icons/MyIcons';
 
@@ -14,164 +18,128 @@ interface ITags {
   id: number;
   name: string;
 }
-interface IProduct {
+
+interface ProductList {
   next: string;
   previous: string;
-  results: [
-    {
-      id: number;
-      name: string;
-      description: string;
-      price: number;
-      capacity: number;
-      tags: ITags[];
-      avgRate: number;
-      reviewCount: number;
-      created: Date;
-    },
-  ];
+  results: Product[];
+  cursor: string;
 }
-const DUMMY_DATA = {
-  results: [
-    {
-      id: 1,
-      name: '크림',
-      description: '촉촉하고 부드러운 보습감을 부여하는 마일드 크림',
-      price: 25000,
-      capacity: 250,
-      tags: [
-        {
-          id: 1,
-          name: '올인원',
-        },
-        {
-          id: 2,
-          name: '클렌저',
-        },
-        {
-          id: 3,
-          name: '마일드',
-        },
-        {
-          id: 6,
-          name: '크림',
-        },
-      ],
-      avgRate: 4.5,
-      reviewCount: 4,
-      created: new Date(),
-    },
-    {
-      id: 1,
-      name: '크림',
-      description: '촉촉하고 부드러운 보습감을 부여하는 마일드 크림',
-      price: 25000,
-      capacity: 250,
-      tags: [
-        {
-          id: 1,
-          name: '올인원',
-        },
-        {
-          id: 2,
-          name: '클렌저',
-        },
-        {
-          id: 3,
-          name: '마일드',
-        },
-        {
-          id: 6,
-          name: '크림',
-        },
-      ],
-      avgRate: 4.5,
-      reviewCount: 4,
-      created: new Date(),
-    },
-    {
-      id: 1,
-      name: '크림',
-      description: '촉촉하고 부드러운 보습감을 부여하는 마일드 크림',
-      price: 25000,
-      capacity: 250,
-      tags: [
-        {
-          id: 1,
-          name: '올인원',
-        },
-        {
-          id: 2,
-          name: '클렌저',
-        },
-        {
-          id: 3,
-          name: '마일드',
-        },
-        {
-          id: 6,
-          name: '크림',
-        },
-      ],
-      avgRate: 4.5,
-      reviewCount: 4,
-      created: new Date(),
-    },
-  ],
-};
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  capacity: number;
+  tags: ITags[];
+  avgRate: number;
+  reviewCount: number;
+  created: Date;
+  thumbnail: string;
+}
+
+async function getProductList(cursor: string): Promise<ProductList> {
+  const { data } = await instance.get(
+    cursor
+      ? `/v1/product/?cursor=${cursor}&?page_size=10`
+      : `/v1/product/?page_size=10`,
+  );
+  return data;
+}
 
 function ProductListPage() {
+  const {
+    data: productListData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    ['product-list'],
+    ({ pageParam }) => {
+      return getProductList(pageParam);
+    },
+    {
+      getNextPageParam: (lastPage, allPage) => lastPage.cursor || undefined,
+    },
+  );
   return (
     <>
-      <Container pt={LAYOUT.HEADER.HEIGHT} pb="1.5rem" bg="gray.100">
-        {DUMMY_DATA.results.map((product) => (
-          <Box
-            key={product.id}
-            boxShadow="0 0 10px #1A1A1A1A"
-            borderRadius="20px"
-            my="2rem"
-            bg="white"
-          >
-            <Img
-              src="./images/dummyImg/DummyProductList.png"
-              alt="상품이미지"
-            />
-            <Flex flexDirection="column" px="1.5rem" pt="1.5rem">
-              <Flex gap="5px">
-                <Text textStyle="sm_wb">{product.name}</Text>
-                <Text textStyle="sm_wn_cg600">{product.capacity}ml</Text>
-              </Flex>
-              <Flex mt=".7rem">
-                <Text textStyle="sl_wb_cp">{product.price}</Text>
-                <Text as="span" textStyle="md">
-                  원
-                </Text>
-              </Flex>
-              <Flex alignItems="center" gap="3px">
-                <RatingStarIcon color="primary.500" />
-                <Text textStyle="sm_wb">{product.avgRate}</Text>
-                <Text textStyle="sm_wn_cg700">
-                  (리뷰 {product.reviewCount}개)
-                </Text>
-              </Flex>
-              <Flex textStyle="sm_wn_cg700" gap="5px" mt="1rem">
-                {product.tags.map((tag) => (
-                  <Text key={tag.id}>#{tag.name}</Text>
-                ))}
-              </Flex>
-              <Flex pt="1rem" pb="2rem" gap=".7rem">
-                <Button variant="primaryButton" fontSize="md" flexGrow="1">
-                  <Link href="/">바로구매</Link>
-                </Button>
-                <Button variant="whiteButton" fontSize="md" flexGrow="1">
-                  <Link href="/">장바구니</Link>
-                </Button>
-              </Flex>
-            </Flex>
-          </Box>
-        ))}
-        <ScrollToTop />
-      </Container>
+      {isFetching && (
+        <h3 style={{ position: 'fixed', top: '5px', right: '5px' }}>
+          Fetching...
+        </h3>
+      )}
+      <InfiniteScroll
+        loadMore={() => {
+          fetchNextPage();
+        }}
+        hasMore={hasNextPage}
+      >
+        <Container pt={LAYOUT.HEADER.HEIGHT} pb="1.5rem" bg="gray.100">
+          {productListData &&
+            productListData.pages.map((productList) =>
+              productList.results.map((product) => {
+                return (
+                  <Box
+                    key={product.id}
+                    boxShadow="0 0 10px #1A1A1A1A"
+                    borderRadius="20px"
+                    my="2rem"
+                    bg="white"
+                  >
+                    <Img
+                      src="./images/dummyImg/DummyProductList.png"
+                      alt="상품이미지"
+                    />
+                    <Flex flexDirection="column" px="1.5rem" pt="1.5rem">
+                      <Flex gap="5px">
+                        <Text textStyle="sm_wb">{product.name}</Text>
+                        <Text textStyle="sm_wn_cg600">
+                          {product.capacity}ml
+                        </Text>
+                      </Flex>
+                      <Flex mt=".7rem">
+                        <Text textStyle="sl_wb_cp">{product.price}</Text>
+                        <Text as="span" textStyle="md">
+                          원
+                        </Text>
+                      </Flex>
+                      <Flex alignItems="center" gap="3px">
+                        <RatingStarIcon color="primary.500" />
+                        <Text textStyle="sm_wb">{product.avgRate}</Text>
+                        <Text textStyle="sm_wn_cg700">
+                          (리뷰 {product.reviewCount}개)
+                        </Text>
+                      </Flex>
+                      <Flex textStyle="sm_wn_cg700" gap="5px" mt="1rem">
+                        {product.tags.map((tag) => (
+                          <Text key={tag.id}>#{tag.name}</Text>
+                        ))}
+                      </Flex>
+                      <Flex pt="1rem" pb="2rem" gap=".7rem">
+                        <Button
+                          variant="primaryButton"
+                          fontSize="md"
+                          flexGrow="1"
+                        >
+                          <Link href="/">바로구매</Link>
+                        </Button>
+                        <Button
+                          variant="whiteButton"
+                          fontSize="md"
+                          flexGrow="1"
+                        >
+                          <Link href="/">장바구니</Link>
+                        </Button>
+                      </Flex>
+                    </Flex>
+                  </Box>
+                );
+              }),
+            )}
+          <ScrollToTop />
+        </Container>
+      </InfiniteScroll>
     </>
   );
 }

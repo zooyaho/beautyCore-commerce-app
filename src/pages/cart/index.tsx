033309b8b76c1fc2daf612271fee47c84dd035b0
zooyaho <jiwoo1268@naver.com>
2022-10-22 18:32:13 +0900

    feat: Add user get api

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,16 +1,21 @@
 import Head from 'next/head';
 
+import { useGetUserMe } from '@apis/user/userApi.query';
+
 import CartPage from '@components/CartPage';
 import CardLayout from '@components/common/@Layout/CardLayout';
 import CommonLayout from '@components/common/@Layout/CommonLayout';
 
 function Cart() {
+  const { data: userData } = useGetUserMe();
   return (
     <>
       <Head>
         <title>Beauty Core | cart</title>
       </Head>
-      <CardLayout content={<CommonLayout content={<CartPage />} />} />
+      <CardLayout
+        content={<CommonLayout content={<CartPage userId={userData?.id} />} />}
+      />
     </>
   );
 }

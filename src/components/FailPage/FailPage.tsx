38d99ecacafcf6865007735e1df4c05a27bb26f0
zooyaho <jiwoo1268@naver.com>
2022-11-07 18:12:16 +0900

    feat: Add a payment failure page style

---
+++ src/components/FailPage/FailPage.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { Button, Center, Flex, Text } from '@chakra-ui/react';
+
+function FailPage() {
+  console.log('❌tposspay fail page!!!');
+  const { query } = useRouter();
+  return (
+    <Flex
+      h="80vh"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Text as="h2" fontSize="24px" mb="1rem" textAlign="center">
+        주문이 정상적으로
+        <br />
+        <strong>완료되지 않았습니다.</strong>
+      </Text>
+      <Text color="gray.600" mb="2rem">
+        이용에 불편을 드려 죄송합니다.
+      </Text>
+      <Flex
+        flexDirection="column"
+        alignItems="left"
+        justifyContent="center"
+        bgColor="gray.100"
+        w="80%"
+        p="2rem"
+        textStyle="ss_wb_cg700"
+      >
+        <Flex>
+          <Text>결제 실패 사유: </Text>
+          <Text color="primary.600" ml=".5rem">
+            {query.message}
+          </Text>
+        </Flex>
+        <Flex>
+          <Text>결제 번호: </Text>
+          <Text ml=".5rem">{query.orderId}</Text>
+        </Flex>
+        <Text mt="1rem">확인 후 장바구니에서 다시 주문해주세요.</Text>
+      </Flex>
+      <Flex w="100%" gap="1rem" flexDirection="column" alignItems="center">
+        <Button variant="primaryButton" size="lg" w="60%" mt="2rem">
+          <Link href="/cart">
+            <Center as="a" w="100%" h="100%">
+              장바구니로 가기
+            </Center>
+          </Link>
+        </Button>
+        <Button variant="whiteButton" size="lg" w="60%">
+          <Link href="/home">
+            <Center as="a" w="100%" h="100%">
+              홈으로 이동
+            </Center>
+          </Link>
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default FailPage;
--- src/pages/tosspayment/fail.tsx
+++ src/pages/tosspayment/fail.tsx
@@ -1,6 +1,18 @@
+import Head from 'next/head';
+
+import FailPage from '@components/FailPage';
+import CardLayout from '@components/common/@Layout/CardLayout';
+import CommonLayout from '@components/common/@Layout/CommonLayout';
+
 const Fail = () => {
-  console.log('❌tposspay fail page!!!');
-  return <></>;
+  return (
+    <>
+      <Head>
+        <title>Beauty Core | tosspayment-fail</title>
+      </Head>
+      <CardLayout content={<CommonLayout content={<FailPage />} />} />
+    </>
+  );
 };
 
 export default Fail;

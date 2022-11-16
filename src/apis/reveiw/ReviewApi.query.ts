459a2789ec52dc89062131901318b98eb75610f3
zooyaho <jiwoo1268@naver.com>
2022-11-16 20:46:34 +0900

    chore: Change parameter

--- src/apis/reveiw/ReviewApi.query.ts
+++ src/apis/reveiw/ReviewApi.query.ts
@@ -2,7 +2,18 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getReviewList } from './ReviewListApi';
 
-export const useGetReviewList = () => {
-  const { data } = useQuery(['review-list'], getReviewList);
-  return data?.results;
+export const useGetReviewList = (
+  page: number,
+  page_size: number,
+  userId?: number,
+) => {
+  const { data, isLoading } = useQuery(
+    ['review-list', page],
+    () => {
+      if (userId) return getReviewList(page, page_size, userId);
+      else return getReviewList(page, page_size);
+    },
+    { enabled: !!page },
+  );
+  return { data, isLoading };
 };
